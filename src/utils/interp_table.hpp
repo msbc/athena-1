7810f897523273343d1c0e41d0ef39589d89567f
Matt Coleman <truth.is.evolution@gmail.com>
2022-07-18 11:26:42 -0400

    Added ND interpolation

--- src/utils/interp_table.hpp
+++ src/utils/interp_table.hpp
@@ -17,6 +17,30 @@
 #include "../athena.hpp"         // Real
 #include "../athena_arrays.hpp"  // AthenaArray
 
+Real inline interp_nd(int nd, int *shape, Real *indices, Real *data,
+                      int *strides=nullptr);
+
+class InterpTable1D {
+ public:
+  InterpTable1D() = default;
+  InterpTable1D(const int nvar, const int nx);
+
+  void SetSize(const int nvar, const int nx);
+  Real interpolate(int nvar, Real x);
+  int nvar();
+  AthenaArray<Real> data;
+  void SetXlim(Real xmin, Real xmax);
+  void GetXlim(Real &xmin, Real &xmax);
+  void GetSize(int &nvar, int &nx);
+
+ private:
+  int nvar_;
+  int nx_;
+  Real xmin_;
+  Real xmax_;
+  Real xnorm_;
+};
+
 class InterpTable2D {
  public:
   InterpTable2D() = default;
@@ -42,6 +66,44 @@ class InterpTable2D {
   Real x2min_;
   Real x2max_;
   Real x2norm_;
+};
+
+class InterpTableND {
+ public:
+  InterpTableND() = default;
+  InterpTableND(AthenaArray<int> shape);
+  InterpTableND(AthenaArray<Real> data);
+  InterpTableND(int nvar, AthenaArray<int> shape);
+  InterpTableND(int nvar, int n, int *shape);
+
+  void SetSize(AthenaArray<int> shape);
+  void SetSize(int nvar, AthenaArray<int> shape);
+  void SetSize(int nvar, int n, int *shape);
+
+  Real interpolate(int nvar, AthenaArray<Real> shape);
+  Real interpolate(int nvar, Real *shape);
+  int nvar();
+  AthenaArray<Real> data;
+  void SetLimits(AthenaArray<Real> low, AthenaArray<Real> high);
+  void SetLimits(Real *low, Real *high);
+  int getShape(int n);
+  Real getMin(int n);
+  Real getMax(int n);
+  int getNdim();
+  int getNvar();
+  void AllocateData();
+
+ private:
+  int nvar_;
+  int ndim_;
+  int offset_;
+  AthenaArray<int> shape_;
+  AthenaArray<int> strides_;
+  AthenaArray<Real> minimums_;
+  AthenaArray<Real> maximums_;
+  AthenaArray<Real> norms_;
+  AthenaArray<Real> location_;
+  void InitAux_();
 };
 
 class EosTable {
